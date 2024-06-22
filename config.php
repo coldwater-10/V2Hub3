5b462308dd62887a8545d7c8ed45f1e6ca467dc8
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2024-06-22 13:51:08 +0330

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$subscription_urls = [
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/vmess_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/trojan_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/vless_iran.txt",
+];
