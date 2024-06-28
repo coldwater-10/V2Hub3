c5b37cd5d204400cfee4cd247116ebec26434f02
coldwater-10 <91764021+coldwater-10@users.noreply.github.com>
2024-06-28 20:24:58 +0330

    Update config.php

--- config.php
+++ config.php
@@ -5,4 +5,6 @@ $subscription_urls = [
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/trojan_iran.txt",
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/vless_iran.txt",
+  "https://dy.smjc.top/api/v1/client/subscribe?token=b12651a704cf680e5794f61827a46262",
+  "https://dy.smjc.top/api/v1/client/subscribe?token=5bd7e01f6a299be186e5cd44f9c6c150",
 ];
